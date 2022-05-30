b28bf300ffd26e9533bc93dcfbfd54f7b3515975
Marat Garipov <maratgaripov1985@gmail.com>
2022-05-30 18:31:53 +0300

    decrypt edited

--- src/Cypher.java
+++ src/Cypher.java
@@ -12,30 +12,29 @@ public class Cypher {
             'ъ', 'ы', 'ь', 'э', 'я', '.', ',', '«', '»', '"', '\'', ':', '!', '?', ' '};
 
 
-    public static void encrypt(String path, int key){
+    public static void encrypt(String path, int key) {
 
         final List<Character> sourceText = new ArrayList<>();
 
-        try(FileReader input = new FileReader(path);) {
+        try (FileReader input = new FileReader(path);) {
             while (input.ready()) {
                 char ch = (char) (input.read());
                 sourceText.add(ch);
             }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
         }
-        catch (IOException e){
-            System.out.println(e.getMessage());
-        }
-        char[] encryptedText = new char[sourceText.size()];
+        char[] encryptedText = new char[sourceText.size()-1];
         int index = 0;
-        for (char ch:sourceText) {
-            int i = 0;
-            while (true){
+        for (char ch : sourceText) {
+            int i = -1;
+            while (true) {
                 i++;
-                if (i == ALPHABET.length){
+                if (i == ALPHABET.length) {
                     break;
                 }
-                if (ch == ALPHABET[i]){
-                    encryptedText[index] = ALPHABET[(i + key)%40];
+                if (ch == ALPHABET[i]) {
+                    encryptedText[index] = ALPHABET[(i + key) % 40];
                     index++;
                     break;
                 }
@@ -43,56 +42,58 @@ public class Cypher {
         }
         Path sourceFile = Path.of(path);
         String inputFileName = sourceFile.getFileName().toString();
-        String outputFileName = inputFileName.substring(0, inputFileName.length() - 4) +  "encrypted.txt";
+        String outputFileName = inputFileName.substring(0, inputFileName.length() - 4) + "encrypted.txt";
         String outputPath = sourceFile.getParent().toString() + "\\" + outputFileName;
-        try(FileWriter output = new FileWriter(outputPath);){
-        output.write(encryptedText);}
-        catch (IOException e){
-            System.out.println(e.getMessage());
+        try (FileWriter output = new FileWriter(outputPath);) {
+            output.write(encryptedText);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
         }
     }
 
-    public static void decrypt(String path, int key){
+    public static void decrypt(String path, int key) {
 
         final List<Character> sourceText = new ArrayList<>();
 
-        try(FileReader input = new FileReader(path);) {
+        try (FileReader input = new FileReader(path);) {
             while (input.ready()) {
                 char ch = (char) (input.read());
                 sourceText.add(ch);
             }
-        }
-        catch (IOException e){
-            System.out.println(e.getMessage());
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
         }
         char[] decryptedText = new char[sourceText.size()];
         int index = 0;
-        for (char ch:sourceText) {
-            int i = 0;
-            while (ch != ALPHABET[i]){
+        for (char ch : sourceText) {
+            int i = -1;
+            while (true) {
                 i++;
-                if (i == ALPHABET.length){
-                    System.out.println("Символа '" + ch + "' нет в алфавите");
+                if (i == ALPHABET.length) {
+                    System.err.println("Символа '" + ch + "' нет в алфавите");
+                    break;
+                }
+                if (ch == ALPHABET[i]) {
+                    decryptedText[index] = ALPHABET[(40 + i - key)%40];
+                    index++;
                     break;
                 }
             }
-            decryptedText[index] = ALPHABET[(40 + i - key)%40];
-            index++;
         }
         Path sourceFile = Path.of(path);
         String inputFileName = sourceFile.getFileName().toString();
-        String outputFileName = inputFileName.substring(0, inputFileName.length() - 4) +  "decrypted.txt";
+        String outputFileName = inputFileName.substring(0, inputFileName.length() - 4) + "decrypted.txt";
         String outputPath = sourceFile.getParent().toString() + "\\" + outputFileName;
-        try(FileWriter output = new FileWriter(outputPath);){
-            output.write(decryptedText);}
-        catch (IOException e){
-            System.out.println(e.getMessage());
+        try (FileWriter output = new FileWriter(outputPath);) {
+            output.write(decryptedText);
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
         }
     }
 
-    public static void main(String[] args){
-        encrypt("C:\\Distrib\\JAVA\\sourceee\\123.txt", 1);
-        decrypt("C:\\Distrib\\JAVA\\sourceee\\123encrypted.txt", 1);
+    public static void main(String[] args) {
+        encrypt("C:\\Distrib\\JAVA\\sourceee\\123.txt", 3);
+        decrypt("C:\\Distrib\\JAVA\\sourceee\\123encrypted.txt", 3);
     }
 
 }
